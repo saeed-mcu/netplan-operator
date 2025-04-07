ae804c7e47dcc74df12bbd4bc9e797e066ef3412
Saeed Padari <sam137115@gmail.com>
2025-04-07 13:09:47 +0330

    log: add nodename

--- internal/controller/netplanconfig_controller.go
+++ internal/controller/netplanconfig_controller.go
@@ -113,7 +113,7 @@ func (r *NetplanConfigReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		logger.Info("All node selected")
 
 	} else if netConfig.Spec.NodeName != nodeName {
-		logger.Info("Node Selector not matched")
+		logger.Info("Node Selector not matched", "CRD", netConfig.Spec.NodeName, "nodeName", nodeName)
 		return ctrl.Result{}, nil
 	}
 
